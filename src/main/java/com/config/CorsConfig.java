c9f480e4e721fefd73c1d883f2d08a18286fbaff
Bianca Leticia <bianca_neckel@hotmail.com>
2026-03-28 16:06:15 -0300

    Update CorsConfig.java

--- src/main/java/com/config/CorsConfig.java
+++ src/main/java/com/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:55099", "http://localhost:8080") 
+                        .allowedOrigins("http://localhost:55099") // origem do Flutter Web
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
